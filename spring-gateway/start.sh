35454cf2ba98ddaeebf1c7a79deb43d29553e66b
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-04-11 21:16:37 +0300

    Fix

--- spring-gateway/start.sh
+++ spring-gateway/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
 /usr/bin/java -Xmx256m -Xss512k -XX:-UseContainerSupport \
-              -jar /app/app.jar
+              -jar /app/app.jar \
+              --SPRING_DEMO_1_URL: http://0.0.0.0:8081 \
+              --SPRING_DEMO_2_URL: http://0.0.0.0:8082
