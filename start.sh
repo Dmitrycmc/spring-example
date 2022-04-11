b71bc1df5a4306ad58af25a8e59c68b8894adb6e
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-04-11 20:38:20 +0300

    Fix

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 
-/usr/bin/java -Xmx256m -Xss512k -XX:-UseContainerSupport \
-              -jar /app/app1.jar & \
+/usr/bin/java -jar /app/app1.jar & \
 
-/usr/bin/java -Xmx256m -Xss512k -XX:-UseContainerSupport \
-              -jar /app/app2.jar & \
+/usr/bin/java -jar /app/app2.jar & \
 
-/usr/bin/java -Xmx256m -Xss512k -XX:-UseContainerSupport \
-              -jar /app/gateway.jar \
+/usr/bin/java -jar /app/gateway.jar \
               --SPRING_DEMO_1_URL: http://localhost:8081 \
               --SPRING_DEMO_2_URL: http://localhost:8082
